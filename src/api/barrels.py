5e791153c662685cf8cbead208245685ccbc378a
Jessica Luu <jessica.luu0002@gmail.com>
2024-04-08 09:55:36 -0700

    v1: subtract gold when barrels delivered

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -22,15 +22,15 @@ class Barrel(BaseModel):
 @router.post("/deliver/{order_id}")
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     """ """
-    # Calculate the total ml of green potion delivered
+    # Calculate the total ml of green potion delivered and gold
     total_ml_delivered = sum(barrel.ml_per_barrel * barrel.quantity for barrel in barrels_delivered if "GREEN" in barrel.sku)
-
+    new_gold = sum(barrel.price * barrel.quantity for barrel in barrels_delivered if "GREEN" in barrel.sku)
     with db.engine.begin() as connection:
         sql_to_execute = """
             UPDATE global_inventory
-            SET num_green_ml = num_green_ml + :ml_added
+            SET num_green_ml = num_green_ml + :ml_added, gold = gold - :sub_gold
         """
-        connection.execute(sqlalchemy.text(sql_to_execute), {"ml_added" : total_ml_delivered})
+        connection.execute(sqlalchemy.text(sql_to_execute), {"ml_added" : total_ml_delivered, "sub_gold" : new_gold})
 
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
